3f4f199641ccd35acdc0cc74d41e3d4c713834c5
mimiazato <lucas_miazato@hotmail.com>
2022-02-24 22:39:16 -0300

    add media

--- modelo_projeto_01/src/controller/apiv.py
+++ modelo_projeto_01/src/controller/apiv.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 
 df = pd.read_csv('dataset.csv')
+df1 = df.groupby('car_make').agg({'car_value':'mean'})
 
 def post(request):
     response = {
@@ -17,25 +18,26 @@ def get():
     return dados
 
 def get1():
-    df.to_json('test.json', orient="records")
-    with open('test.json', encoding='utf-8') as meu_json:
-        dados = json.load(meu_json)
+    print(df1)
+    df1.to_json('test.json1', orient="records")
+    with open('test.json1', encoding='utf-8') as meu_json1:
+        dados = json.load(meu_json1)
     return dados
 
 def get2():
-    df.to_json('test.json', orient="records")
-    with open('test.json', encoding='utf-8') as meu_json:
-        dados = json.load(meu_json)
+    df.to_json('test.json2', orient="records")
+    with open('test.json2', encoding='utf-8') as meu_json2:
+        dados = json.load(meu_json2)
     return dados
 
 def get3():
-    df.to_json('test.json', orient="records")
-    with open('test.json', encoding='utf-8') as meu_json:
-        dados = json.load(meu_json)
+    df.to_json('test.json3', orient="records")
+    with open('test.json3', encoding='utf-8') as meu_json3:
+        dados = json.load(meu_json3)
     return dados
 
 def get4():
-    df.to_json('test.json', orient="records")
-    with open('test.json', encoding='utf-8') as meu_json:
-        dados = json.load(meu_json)
+    df.to_json('test.json4', orient="records")
+    with open('test.json4', encoding='utf-8') as meu_json4:
+        dados = json.load(meu_json4)
     return dados
