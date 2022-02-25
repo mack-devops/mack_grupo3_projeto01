bb117c918344ff50dbeab71843c43e3218b73ed8
mimiazato <lucas_miazato@hotmail.com>
2022-02-24 22:07:42 -0300

    test

--- modelo_projeto_01/src/controller/apiv.py
+++ modelo_projeto_01/src/controller/apiv.py
@@ -2,26 +2,40 @@ import pandas as pd
 import json
 
 df = pd.read_csv('dataset.csv')
-df.to_json('test.json', orient = "records")
-with open('test.json', encoding='utf-8') as meu_json:
-    dados = json.load(meu_json)
-
-# TODO: ESTE É UM EXEMPLO DE IMPLEMENTAÇÃO DA CONTROLLER
-
-def get():
-    # TODO: IMPLEMENTE AQUI
-    
-   # response = {
-   #     "data: print(test.json)"
-   # }
-
-    return dados
 
 def post(request):
-    # TODO: IMPLEMENTE AQUI
-
     response = {
         "data": "VALOR RETORNADO PELA CONTROLLER QUANDO A SOLICITAÇÃO FOR POST"
     }
-
     return response
+
+
+def get():
+    df.to_json('test.json', orient="records")
+    with open('test.json', encoding='utf-8') as meu_json:
+        dados = json.load(meu_json)
+    return dados
+
+def get1():
+    df.to_json('test.json', orient="records")
+    with open('test.json', encoding='utf-8') as meu_json:
+        dados = json.load(meu_json)
+    return dados
+
+def get2():
+    df.to_json('test.json', orient="records")
+    with open('test.json', encoding='utf-8') as meu_json:
+        dados = json.load(meu_json)
+    return dados
+
+def get3():
+    df.to_json('test.json', orient="records")
+    with open('test.json', encoding='utf-8') as meu_json:
+        dados = json.load(meu_json)
+    return dados
+
+def get4():
+    df.to_json('test.json', orient="records")
+    with open('test.json', encoding='utf-8') as meu_json:
+        dados = json.load(meu_json)
+    return dados
--- modelo_projeto_01/src/infra/http/route/example.py
+++ modelo_projeto_01/src/infra/http/route/example.py
@@ -1,7 +1,5 @@
 import flask
 from flask import jsonify
-
-
 import src.controller.apiv as apiv
 import src.infra.http.auth as auth
 from src.infra.utility.http import Http
@@ -25,6 +23,58 @@ def route(app: flask.app.Flask):
     def request_post_data():
         try:
             response = apiv.post(request)
+
+            return jsonify(response), 200
+        except Exception as err:
+            response = Http.handle_generic_http_error(err)
+
+            return jsonify(response), 500
+
+
+    @app.route('/data1', methods=['GET'])
+    @auth.requires_auth
+    def request_post_data1():
+        try:
+            response = apiv.get1()
+
+            return jsonify(response), 200
+        except Exception as err:
+            response = Http.handle_generic_http_error(err)
+
+            return jsonify(response), 500
+
+
+    @app.route('/data2', methods=['GET'])
+    @auth.requires_auth
+    def request_post_data2():
+        try:
+            response = apiv.get1()
+
+            return jsonify(response), 200
+        except Exception as err:
+            response = Http.handle_generic_http_error(err)
+
+            return jsonify(response), 500
+
+
+    @app.route('/data3', methods=['GET'])
+    @auth.requires_auth
+    def request_post_data3():
+        try:
+            response = apiv.get1()
+
+            return jsonify(response), 200
+        except Exception as err:
+            response = Http.handle_generic_http_error(err)
+
+            return jsonify(response), 500
+
+
+    @app.route('/data4', methods=['GET'])
+    @auth.requires_auth
+    def request_post_data4():
+        try:
+            response = apiv.get1()
 
             return jsonify(response), 200
         except Exception as err:
